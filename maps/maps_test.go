f57e11b987912371caa3faef864d5d27db923b8f
asreal089 <lexx_89@hotmail.com>
2024-02-16 02:10:03 -0300

    Adding maps

---
+++ maps/maps_test.go
@@ -0,0 +1,13 @@
+package maps
+
+import "testing"
+
+func testSearch(t *testing.T) {
+	dict := map[string]string{"test": "its a test"}
+
+	got := Search(dict, "test")
+  want := "its a test"
+	if got != want {
+		t.Errorf("got diff than want")
+	}
+}
