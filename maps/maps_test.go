a908d72f2cd8258d87f51d58d698bc9c7d046d4f
asreal089 <lexx_89@hotmail.com>
2024-02-16 09:12:09 -0300

    Fixing test

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -2,11 +2,11 @@ package maps
 
 import "testing"
 
-func testSearch(t *testing.T) {
+func TestSearch(t *testing.T) {
 	dict := map[string]string{"test": "its a test"}
 
 	got := Search(dict, "test")
-  want := "its a test"
+	want := "its a test"
 	if got != want {
 		t.Errorf("got diff than want")
 	}
